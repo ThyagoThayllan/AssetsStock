da92d6c77ff9ac1ca8b366814d4b2647fdca2095
thyagothayllan <thayllanst@outlook.com>
2024-10-09 17:45:20 -0300

    Criação do formulário de cadastro de produtos - NewProduct

--- src/components/NewProduct/index.jsx
+++ src/components/NewProduct/index.jsx
@@ -1,7 +1,46 @@
-export const NewProduct = () => {
+import styles from "./styles.module.css";
+
+export const NewProduct = ({ categorys }) => {
   return (
     <div>
       <h1>Novo Produto</h1>
+
+      <form>
+        <div className={styles.divForm}>
+          <label htmlFor="model">Modelo:</label>
+          <input
+            type="text"
+            name="model"
+            placeholder="Digite o modelo do produto..."
+          />
+        </div>
+        <div className={styles.divForm}>
+          <label htmlFor="manufacturer">Fabricante:</label>
+          <input
+            type="text"
+            name="manufacturer"
+            placeholder="Digite o fabricante do produto..."
+          />
+        </div>
+        <div className={styles.divForm}>
+          <label htmlFor="category">Categoria:</label>
+          <select name="category">
+            <option value="">Selecionar</option>
+            {categorys.map((category) => (
+              <option value={category}>{category}</option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.divForm}>
+          <label htmlFor="notes">Nota:</label>
+          <textarea
+            name="note"
+            placeholder="Nota sobre o produto..."
+          ></textarea>
+        </div>
+
+        <button className={styles.btn}>Cadastrar</button>
+      </form>
     </div>
   );
 };

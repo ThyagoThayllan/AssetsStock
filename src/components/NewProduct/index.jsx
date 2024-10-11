1f0e2c36139a5fb19d9b4df36428559172a60fe5
thyagothayllan <thayllanst@outlook.com>
2024-10-11 10:03:30 -0300

    Criação da lógica de cadastro de produtos: **registerAsset**

--- src/components/NewProduct/index.jsx
+++ src/components/NewProduct/index.jsx
@@ -1,14 +1,55 @@
 import { useState } from "react";
+import axios from "axios";
 import styles from "./styles.module.css";
 
 export const NewProduct = ({ categorys, product, productFunctions }) => {
   const [registered, setRegistered] = useState(false);
+  const [empty, setEmpty] = useState(false);
+
+  const registerAsset = async (e) => {
+    e.preventDefault();
+
+    if (
+      product.model === "" ||
+      product.manufacturer === "" ||
+      product.category === ""
+    ) {
+      setEmpty(true);
+    } else {
+      try {
+        setEmpty(false);
+
+        await axios.post(
+          "?action=register",
+          {
+            model: product.model,
+            manufacturer: product.manufacturer,
+            category: product.category,
+            note: product.note,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        setRegistered(!registered);
+        productFunctions.setModel("");
+        productFunctions.setManufacturer("");
+        productFunctions.setCategory("");
+        productFunctions.setNote("");
+      } catch (error) {
+        console.error(`Erro: ${error}`);
+      }
+    }
+  };
 
   return (
     <div>
       <h1>Novo produto</h1>
 
-      <form>
+      <form onSubmit={(e) => registerAsset(e)}>
         <div className={styles.divForm}>
           <label htmlFor="model">Modelo:</label>
           <input
@@ -16,8 +57,8 @@ export const NewProduct = ({ categorys, product, productFunctions }) => {
             name="model"
             placeholder="Digite o modelo do produto..."
             onChange={(e) => productFunctions.setModel(e.target.value)}
+            value={product.model}
           />
-          {product.model}
         </div>
         <div className={styles.divForm}>
           <label htmlFor="manufacturer">Fabricante:</label>
@@ -26,38 +67,44 @@ export const NewProduct = ({ categorys, product, productFunctions }) => {
             name="manufacturer"
             placeholder="Digite o fabricante do produto..."
             onChange={(e) => productFunctions.setManufacturer(e.target.value)}
+            value={product.manufacturer}
           />
-          {product.manufacturer}
         </div>
         <div className={styles.divForm}>
           <label htmlFor="category">Categoria:</label>
           <select
             name="category"
             onChange={(e) => productFunctions.setCategory(e.target.value)}
+            value={product.category}
           >
             <option value="">Selecionar</option>
             {categorys.map((category) => (
-              <option value={category}>{category}</option>
+              <option key={category} value={category}>
+                {category}
+              </option>
             ))}
           </select>
-          {product.category}
         </div>
         <div className={styles.divForm}>
-          <label htmlFor="notes">Nota:</label>
+          <label htmlFor="notes">
+            Nota: <small>(opcional)</small>
+          </label>
           <textarea
             name="note"
             placeholder="Nota sobre o produto..."
             onChange={(e) => productFunctions.setNote(e.target.value)}
+            value={product.note}
           ></textarea>
-          {product.note}
         </div>
 
         <div>
-          <button
-            type="button"
-            onClick={() => setRegistered(!registered)}
-            className={styles.btn}
-          >
+          {empty && (
+            <p className={styles.alert}>
+              Preencha todos os campos necessários!
+            </p>
+          )}
+
+          <button type="submit" className={styles.btn}>
             Cadastrar
           </button>
           {registered && (

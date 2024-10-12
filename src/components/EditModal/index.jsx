97102b91ae407208eb483de3fd9339f712a53c40
thyagothayllan <thayllanst@outlook.com>
2024-10-11 21:47:42 -0300

    Criação do model de edição

--- src/components/EditModal/index.jsx
+++ src/components/EditModal/index.jsx
@@ -1,39 +1,139 @@
+import { useState } from "react";
 import styles from "./styles.module.css";
 
-export const EditModal = ({ modal }) => {
+export const EditModal = ({
+  modal,
+  categorys,
+  selectedAsset,
+  editAsset,
+  getAssets,
+  getAsset,
+  action,
+  filter,
+  modelData,
+  categoryData,
+}) => {
+  const [equalField, setEqualField] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  const [model, setModel] = useState(selectedAsset.model);
+  const [manufacturer, setManufacturer] = useState(
+    selectedAsset.manufacturer
+  );
+  const [category, setCategory] = useState(selectedAsset.category);
+  const [note, setNote] = useState(selectedAsset.note);
+
+  const edit = () => {
+    if (
+      model.trim() === selectedAsset.model &&
+      manufacturer.trim() === selectedAsset.manufacturer &&
+      category.trim() === selectedAsset.category &&
+      note.trim() === selectedAsset.note
+    ) {
+      setEqualField(true);
+      return;
+    }
+
+    if (
+      model.trim() === "" ||
+      manufacturer.trim() === "" ||
+      category.trim() === ""
+    ) {
+      setEqualField(false);
+      setIsEmpty(true);
+      return;
+    }
+
+    setIsEmpty(false);
+
+    editAsset(
+      selectedAsset.id,
+      model,
+      manufacturer,
+      category,
+      note,
+      action === "assets"
+        ? getAssets
+        : () =>
+            getAsset(filter, filter === "model" ? modelData : categoryData)
+    );
+  };
+
   return (
     <div className={styles.overlay}>
       <div className={styles.container}>
         <h2>Editar</h2>
 
-        <form>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+
+            edit();
+          }}
+        >
           <div>
             <label htmlFor="model">Modelo:</label>
-            <input type="text" name="model" placeholder="Digite o modelo..." />
+            <input
+              type="text"
+              name="model"
+              placeholder="Digite o modelo..."
+              defaultValue={selectedAsset.model}
+              onChange={(e) => setModel(e.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="marca">Marca:</label>
-            <input type="text" name="marca" placeholder="Digite a marca..." />
+            <input
+              type="text"
+              name="marca"
+              placeholder="Digite a marca..."
+              defaultValue={selectedAsset.manufacturer}
+              onChange={(e) => setManufacturer(e.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="category">Categoria:</label>
-            <select name="category">
-              <option value="">Selecionar</option>
+            <select
+              name="category"
+              defaultValue={selectedAsset.category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              {categorys.map((category) => (
+                <option defaultValue={category} key={category}>
+                  {category}
+                </option>
+              ))}
             </select>
           </div>
           <div>
             <label htmlFor="note">Nota:</label>
-            <textarea name="note" placeholder="Nota sobre o item..."></textarea>
+            <textarea
+              name="note"
+              placeholder="Nota sobre o item..."
+              defaultValue={selectedAsset.note}
+              onChange={(e) => setNote(e.target.value)}
+            ></textarea>
           </div>
 
-          <button className={styles.button}>Editar</button>
+          {equalField && (
+            <p style={{ color: "#f00" }}>Não há mudanças feitas.</p>
+          )}
+          {isEmpty && (
+            <p style={{ color: "#f00" }}>
+              Preencha todos os campos necessários!
+            </p>
+          )}
+
+          <button type="submit" className={styles.button}>
+            Editar
+          </button>
         </form>
 
         <button
           className={styles.closeButton}
-          onClick={() => modal.setModalOpened(!modal.setModalOpened)}
+          onClick={() => modal.setModalOpened(!modal.modalOpened)}
         >
-          <i class="bi bi-x-lg"></i>
+          <i className="bi bi-x-lg"></i>
         </button>
       </div>
     </div>
